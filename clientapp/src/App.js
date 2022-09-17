c9cdfe4cb79ab8a6d6777fa4eddc29c05f5ef977
TheMatiaz0 <TheMatiaz0@protonmail.com>
2022-09-17 16:01:50 +0200

    Fix POST and DELETE Rest APIs

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,32 +1,39 @@
-import NotesList from './components/NotesList';
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
-import Search from './components/Search';
-import Header from './components/Header';
+import NotesList from "./components/NotesList";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import Search from "./components/Search";
+import Header from "./components/Header";
 
 const App = () => {
   // const LOCAL_STORAGE_DATA_NAME = "NoteNough-app-data";
 
-  const defaultNotes = [{
-    key: nanoid(),
-    text: "This is an example note",
-    date: new Date(2022, 7, 24, 10, 9, 5, 10)
-  },
-  {
-    key: nanoid(),
-    text: "This is an example note 2",
-    date: new Date(2023, 10, 1, 10, 9, 5, 10)
-  },
-  {
-    key: nanoid(),
-    text: "This is an example note 3",
-    date: new Date(2024, 10, 1, 10, 9, 5, 10)
-  }];
+  const defaultNotes = [
+    {
+      key: nanoid(),
+      text: "This is an example note",
+      date: new Date(2022, 7, 24, 10, 9, 5, 10),
+    },
+    {
+      key: nanoid(),
+      text: "This is an example note 2",
+      date: new Date(2023, 10, 1, 10, 9, 5, 10),
+    },
+    {
+      key: nanoid(),
+      text: "This is an example note 3",
+      date: new Date(2024, 10, 1, 10, 9, 5, 10),
+    },
+  ];
 
   const parseNoteDates = (parsedNotes) => {
-    return parsedNotes.map(val => {
-      return {...val, date: new Date(val.date)};
-    })
+    return parsedNotes.map((val) => {
+      const createdDate = new Date(val.created);
+      const updatedDate = new Date(val.updated);
+      return {
+        ...val,
+        date: updatedDate > createdDate ? updatedDate : createdDate,
+      };
+    });
   };
 
   const [notes, setNotes] = useState(() => {
@@ -44,7 +51,7 @@ const App = () => {
     return defaultNotes;
   });
 
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
 
   /*
   useEffect(() => {
@@ -52,32 +59,60 @@ const App = () => {
   }, [notes]);
   */
 
+  const addNoteToDatabase = async (text) => {
+    let response = await fetch("http://localhost:8080/api/Notes", {
+      method: "POST",
+      body: JSON.stringify({
+        text: text,
+        created: new Date(),
+        updated: null,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    let data = await response.json();
+    setNotes((notes) => [
+      { key: data.key, text: data.text, date: new Date(data.created) },
+      ...notes,
+    ]);
+  };
+
+  const deleteNoteFromDatabase = async (id) => {
+    await fetch(`http://localhost:8080/api/Notes/${id}`, {
+      method: "DELETE"
+    });
+    let notesExcludingDeleted = [...notes].filter(i => i.key !== id);
+    setNotes(notesExcludingDeleted);
+  };
+
   useEffect(() => {
-    fetch('http://localhost:8080/api/Notes')
-       .then((response) => response.json())
-       .then((data) => {
-          console.log(data);
-          setNotes(parseNoteDates(data));
-       })
-       .catch((err) => {
-          console.log(err.message);
-       });
- }, []);
+    const fetchNotesFromDatabase = async () => {
+      const response = await fetch("http://localhost:8080/api/Notes");
+      const data = await response.json();
+      setNotes(parseNoteDates(data));
+    };
+    fetchNotesFromDatabase();
+  }, []);
 
   const addNote = (text) => {
+    addNoteToDatabase(text);
+    /*
     setNotes([...notes, {
       key: nanoid(),
       text: text,
       date: new Date()
     }]);
-  }
+    */
+  };
 
   const removeNote = (key) => {
-    setNotes(notes.filter(note => note.key !== key));
-  }
+    deleteNoteFromDatabase(key);
+    /* setNotes(notes.filter(note => note.key !== key)); */
+  };
 
   const editNote = (key, newText) => {
-    const specificIndex = notes.findIndex(note => note.key === key);
+    const specificIndex = notes.findIndex((note) => note.key === key);
     const newNotes = [...notes];
     const specificNote = newNotes[specificIndex];
     if (newText !== specificNote.text) {
@@ -85,20 +120,26 @@ const App = () => {
       specificNote.date = new Date();
       setNotes(newNotes);
     }
-  }
+  };
 
-  const filterText = (note) => 
+  const filterText = (note) =>
     note.text.toLowerCase().includes(searchText.toLowerCase());
 
   return (
     <div>
-      <div className='container'>
+      <div className="container">
         <Header />
         <Search handleSearchText={setSearchText} />
-        <NotesList notes={notes.filter(filterText)} searchText={searchText} handleAddNote={addNote} handleRemoveNote={removeNote} handleEditNote={editNote} />
+        <NotesList
+          notes={notes.filter(filterText)}
+          searchText={searchText}
+          handleAddNote={addNote}
+          handleRemoveNote={removeNote}
+          handleEditNote={editNote}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
