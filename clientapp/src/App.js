a983905f814e0f5cc7a5e2b9c11e827dfc5a84c2
TheMatiaz0 <TheMatiaz0@protonmail.com>
2022-09-24 15:27:29 +0200

    Clean up css and js files

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -2,15 +2,11 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import { Route, Routes } from "react-router-dom"
-import Header from "./components/Header";
 
 const App = () => {
-
-
   return (
     <div>
       <div className="container">
-        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
