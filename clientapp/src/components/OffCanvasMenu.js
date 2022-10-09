163e5a4d9e68045de7b620ba4bdd5fca3acf4372
TheMatiaz0 <TheMatiaz0@protonmail.com>
2022-10-09 20:34:39 +0200

    Cleaning up drawer before refactor

--- clientapp/src/components/OffCanvasMenu.js
+++ clientapp/src/components/OffCanvasMenu.js
@@ -1,21 +1,44 @@
 import "react-modern-drawer/dist/index.css";
 import { MdOutlineMail, MdLockOutline } from "react-icons/md";
 import { IoMdEye, IoMdEyeOff } from "react-icons/io";
-import {useState} from "react";
+import { useState } from "react";
 
-const OffCanvasMenu = ({ isOpen, onClose }) => {
+const OffCanvasMenu = ({ isOpen, onClose, onComplete }) => {
   let canvasWidth = "0";
   if (isOpen) {
-    canvasWidth = window.innerWidth < 500 ? "100vw" : "500px";
+    canvasWidth = window.innerWidth < 500 ? "100%" : "500px";
   }
-  const backgroundClr = isOpen ? "rgba(0, 0, 0, 0.4)" : "rgba(0, 0, 0, 0)";
+  const overlayBackgroundColor = isOpen ? "rgba(0, 0, 0, 0.4)" : "rgba(0, 0, 0, 0)";
   const overlayPointerEvents = isOpen ? "all" : "none";
 
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const [isShowingPassword, setShowingPassword] = useState(false);
+  const [isRememberingPassword, setRememberingPassword] = useState(true);
   const toggleShowPassword = () => {
     setShowingPassword((prevState) => !prevState);
   };
-  const ShowPassword = isShowingPassword ? IoMdEyeOff : IoMdEye
+  const toggleRememberPassword = () => {
+    setRememberingPassword((prevState) => !prevState);
+  };
+  const ShowPasswordIcon = isShowingPassword ? IoMdEyeOff : IoMdEye;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!email.trim() || !password.trim()) {
+      return;
+    }
+    /* Encrypt password here */
+    onComplete(email, password);
+  };
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
   return (
     <div>
@@ -24,40 +47,54 @@ const OffCanvasMenu = ({ isOpen, onClose }) => {
           <div className="form-header">
             <div className="form-info">
               <img className="logo" alt="logo" src="favicon.png" />
-              <div className="auth-header">Welcome back</div>
+              <div className="auth-header">Welcome back 👋</div>
             </div>
-            <span onClick={onClose} className="close">&times;</span>
+            <span onClick={onClose} className="close">
+              &times;
+            </span>
           </div>
-          <form action="#">
+          <form action="#" onSubmit={handleSubmit}>
             <div className="input-field">
               <input
                 type="text"
                 placeholder="Enter your email"
                 autoComplete="email"
                 name="Enter your email to log in"
+                onChange={handleChangeEmail}
+                value={email}
                 required
               />
               <MdOutlineMail className="auth-icon" />
             </div>
             <div className="input-field">
               <input
-                type="password"
+                type={isShowingPassword ? "text" : "password"}
                 placeholder="Enter your password"
                 autoComplete="current-password"
                 name="Enter your password to log in"
+                onChange={handleChangePassword}
+                value={password}
                 required
               />
               <MdLockOutline className="auth-icon" />
-              <ShowPassword onClick={toggleShowPassword} className="auth-icon right-icon btn-icon" />
+              <ShowPasswordIcon
+                onClick={toggleShowPassword}
+                className="auth-icon right-icon btn-icon"
+              />
             </div>
             <div className="form-bottom">
               <div className="input-checkbox">
-                <input type="checkbox" id="remember" />
+                <input
+                  checked={isRememberingPassword}
+                  onClick={toggleRememberPassword}
+                  type="checkbox"
+                  id="remember"
+                />
                 <label htmlFor="remember" className="text">
                   Remember password
                 </label>
               </div>
-              <a href="#" className="text">
+              <a href="forgot-password" className="text">
                 Forgot password?
               </a>
             </div>
@@ -70,7 +107,7 @@ const OffCanvasMenu = ({ isOpen, onClose }) => {
       <div
         className="overlay"
         style={{
-          backgroundColor: backgroundClr,
+          backgroundColor: overlayBackgroundColor,
           pointerEvents: overlayPointerEvents,
         }}
         onClick={onClose}
