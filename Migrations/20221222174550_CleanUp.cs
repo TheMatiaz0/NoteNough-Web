50ec49a87124472a8a8fabb52fa6b193057927b4
TheMatiaz0 <TheMatiaz0@protonmail.com>
2022-12-24 16:08:19 +0100

    Revert "Fix BUILD_PATH being undeletable directory"
    This reverts commit 9c7ac58e30cd80e7a97e567394b822c25657c835.

---
+++ Migrations/20221222174550_CleanUp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoteNough.NET.Migrations
+{
+    public partial class CleanUp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
