2d4164907ed8c50b0b6c8cab3fa2293f1d75e0d1
TheMatiaz0 <TheMatiaz0@protonmail.com>
2023-06-04 16:07:35 +0200

    Improve style of website with SVG logo

--- clientapp/src/components/Account/Settings/CollapsibleFormHeader.js
+++ clientapp/src/components/Account/Settings/CollapsibleFormHeader.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { FiChevronDown } from "react-icons/fi";
+import { FiChevronDown, FiChevronRight } from "react-icons/fi";
 
 const CollapsibleFormHeader = ({ text, content, onClick, isSelected }) => {
     return (
         <li className="link">
             <div onClick={onClick} className="accordion">
-                <h2 className="center">{text} <FiChevronDown className="arrow-break arrow-expandable" size="0.8em" /></h2>
+                <h2 className="center">{text} {isSelected ? <FiChevronRight className="arrow-break arrow-expandable" size="0.8em" /> : <FiChevronDown className="arrow-break arrow-expandable" size="0.8em" />}</h2>
                 {isSelected && <div onClick={(e) => e.stopPropagation()} className="content">{content}</div>}
             </div>
         </li>
