fb253716e4dc3a65ccf21c124982db29d759f2a1
TheMatiaz0 <TheMatiaz0@protonmail.com>
2025-02-15 11:48:28 +0100

    Add outline for search box upon focus

--- clientapp/src/components/Core/Search.js
+++ clientapp/src/components/Core/Search.js
@@ -5,11 +5,11 @@ import { MdSearch } from "react-icons/md";
 const Search = ({ handleSearchText }) => {
   return (
     <section role="search" className="search">
-      <MdSearch className="search-icons" size="1.2em" />
+      <MdSearch className="search-icons" size="2em" />
       <input
         onChange={(event) => handleSearchText(event.target.value)}
         type="text"
-        placeholder="Type to search..."
+        placeholder="What do you want to find?"
       />
     </section>
   );
