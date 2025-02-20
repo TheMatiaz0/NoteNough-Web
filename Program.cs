07e2952656280ea6d586a9a66f8054f9eaa890d9
TheMatiaz0 <TheMatiaz0@protonmail.com>
2025-02-20 22:46:12 +0100

    Fix environment variables for C# API

--- Program.cs
+++ Program.cs
@@ -54,7 +54,34 @@ namespace NoteNough.NET
                 };
             });
             builder.Services.AddAuthorization();
-            builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres_Db")));
+
+            var connectionString = builder.Configuration.GetConnectionString("Postgres_Db");
+            if (string.IsNullOrEmpty(connectionString)) // didn't found connection string at appsettings.json
+            {
+                var postgresServer = Environment.GetEnvironmentVariable("POSTGRES_DEVELOPMENT_SERVER") ?? "localhost";
+                var postgresUser = Environment.GetEnvironmentVariable("POSTGRES_USER") ?? "admin";
+                var postgresPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD") ?? "root1234";
+
+                connectionString = $"Server={postgresServer};Database=postgres;Port=5432;Timeout=30;CommandTimeout=60;Username={postgresUser};Password={postgresPassword}";
+            }
+            if (string.IsNullOrEmpty(jwtConfig.SecurityKey))
+            {
+                var envSecurityKey = Environment.GetEnvironmentVariable("JWT_SECURITY_KEY") ?? "YourLocalDevelopmentKeyHere";
+                jwtConfig.SecurityKey = envSecurityKey;
+            }
+            if (string.IsNullOrEmpty(jwtConfig.Issuer))
+            {
+                var envIssuer = Environment.GetEnvironmentVariable("APP_URL") ?? "http://localhost:8080";
+                jwtConfig.Issuer = envIssuer;
+            }
+            if (string.IsNullOrEmpty(jwtConfig.Audience))
+            {
+                var envAudience = Environment.GetEnvironmentVariable("DOMAIN_URL") ?? "localhost:8080";
+                jwtConfig.Audience = envAudience;
+            }
+
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
             builder.Services.AddDataProtection()
                 .PersistKeysToDbContext<AppDbContext>()
                 .SetApplicationName("NoteNough");
