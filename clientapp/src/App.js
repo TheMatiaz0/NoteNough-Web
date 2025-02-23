4e7deb9f0a318eb1b3768b8cb8f5473eee89f720
TheMatiaz0 <TheMatiaz0@protonmail.com>
2025-02-23 13:28:54 +0100

    Add prototype logging on network or API problems

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -5,36 +5,53 @@ import ProtectedRoute from "./components/Router/ProtectedRoute.js";
 import { useEffect, useState } from "react";
 
 const App = () => {
-    const ROOT_AUTHENTICATION_URL = `${process.env.REACT_APP_ROOT_URL}/api/auth`;
+    const [errorMessage, setErrorMessage] = useState("");
     const [user, setUser] = useState(null);
     const [userLoggedIn, setUserLoggedIn] = useState(false);
 
+    const ROOT_AUTHENTICATION_URL = `${process.env.REACT_APP_ROOT_URL}/api/auth`;
+    const GENERIC_ERROR_MESSAGE = "An error occured. Please try again."
+    const NETWORK_ERROR_MESSAGE = "Network error. Please check your connection."
+
     useEffect(() => {
-        fetchUser();
+        fetchUser(true);
     }, [userLoggedIn]);
 
     const authorize = async ({ isLoggingIn, email, password, isRememberPassword }) => {
         const urlName = isLoggingIn ? "login" : "register";
 
-        const response = await fetch(`${ROOT_AUTHENTICATION_URL}/${urlName}`, {
-            method: 'POST',
-            credentials: "include",
-            headers: {
-                'Content-Type': process.env.REACT_APP_FETCH_TYPE,
-            },
-            body: JSON.stringify(
-                {
-                    email: email,
-                    password: password,
-                    rememberMe: isRememberPassword
-                }),
-        });
-
-        await fetchUser();
-        return response.ok;
+        try {
+            const response = await fetch(`${ROOT_AUTHENTICATION_URL}/${urlName}`, {
+                method: 'POST',
+                credentials: "include",
+                headers: {
+                    'Content-Type': process.env.REACT_APP_FETCH_TYPE,
+                },
+                body: JSON.stringify(
+                    {
+                        email: email,
+                        password: password,
+                        rememberMe: isRememberPassword
+                    }),
+            });
+            const data = await response.json();
+    
+            if (!response.ok) {
+              setErrorMessage(data.message || GENERIC_ERROR_MESSAGE);
+              return false;
+            }
+      
+            await fetchUser();
+            setErrorMessage("");
+            return true;
+        }
+        catch (error) {
+            setErrorMessage(NETWORK_ERROR_MESSAGE);
+            return false;
+        }
     }
 
-    const fetchUser = async () => {
+    const fetchUser = async (silently) => {
         try {
             const url = `${ROOT_AUTHENTICATION_URL}/user`;
             const response = await fetch(url, {
@@ -44,13 +61,22 @@ const App = () => {
                     'Content-Type': process.env.REACT_APP_FETCH_TYPE,
                 }
             });
+
             const content = await response.json();
+
+            if (!response.ok) {
+                setErrorMessage(content.message || GENERIC_ERROR_MESSAGE);
+                return;
+            }
             setUser({ email: content.email });
             setUserLoggedIn(true);
         }
-        catch (e) {
+        catch (error) {
             setUser(null);
             setUserLoggedIn(false);
+            if (!silently) {
+                setErrorMessage(NETWORK_ERROR_MESSAGE);
+            }
         }
     }
 
@@ -120,6 +146,12 @@ const App = () => {
 
     return (
         <div>
+            {errorMessage && (
+                <div className="error-message">
+                    <p>{errorMessage}</p>
+                </div>
+            )}
+
             <Routes>
                 <Route path="/" element={<Home onAuthorize={authorize} user={user} onLogout={logoutUser} userLoggedIn={userLoggedIn} />} />
                 <Route path="/account" element={
